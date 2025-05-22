ec31e2efd2518cd229bc9bf5c4ddac5ef14888fe
LFMineiro <lflmalbuquerque3@gmail.com>
2025-05-21 22:52:03 -0300

    feat: Dashboard Page w/ Add Team Function

---
+++ app/dashboard.tsx
@@ -0,0 +1,90 @@
+import AddTeamModal from "@/components/Dashboard/modalAddTeam";
+import { useState } from "react";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+
+export default function Dashboard() {
+
+
+  const [modalTeamVisible, setModalTeamVisible] = useState(false);
+  const [modalPlayerVisible, setModalPlayerVisible] = useState(false);
+
+  const [teamName, setTeamName] = useState("")
+
+  const handleAddTeam = () => {
+    console.log("Equipe adicionada:", teamName);
+    setModalTeamVisible(false);
+    setTeamName("");
+  }
+
+
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.section}>
+        <Text style={styles.title}>Resultados dos Jogos</Text>
+        
+        <View style={styles.resultCard}>
+          <Text>Computadores 2 x 1 Cartógrafos</Text>
+        </View>
+
+        <View style={styles.buttonRow}>
+          <Button title="Adicionar Jogador" onPress={() => {setModalPlayerVisible(true)}} />
+          <Button title="Adicionar Equipe" onPress={() => {setModalTeamVisible(true)}} />
+        </View>
+      </View>
+
+      {/* Equipes */}
+      <View style={styles.section}>
+        <Text style={styles.title}>Equipes do Torneio</Text>
+
+        <View style={styles.teamCard}>
+          <Text>Computadores</Text>
+        </View>
+        <View style={styles.teamCard}>
+          <Text>Cartógrafos</Text>
+        </View>
+      </View>
+      {/* Modais */}
+      <AddTeamModal 
+      visible={modalTeamVisible}
+      onClose={() => {setModalTeamVisible(false)}}
+      onSave={handleAddTeam}
+      value={teamName}
+      onChange={setTeamName}
+        />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+  },
+  section: {
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  resultCard: {
+    backgroundColor: "#e6e6e6",
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  teamCard: {
+    backgroundColor: "#cde",
+    padding: 10,
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  buttonRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+    gap: 10,
+  },
+});
---
+++ components/Dashboard/modalAddTeam.tsx
@@ -0,0 +1,79 @@
+import { Modal, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+
+type Props = {
+  visible: boolean;
+  onClose: () => void;
+  onSave: () => void;
+  value: string;
+  onChange: (text: string) => void;
+};
+
+export default function AddTeamModal({ visible, onClose, onSave, value, onChange }: Props) {
+
+
+  return (
+    <Modal visible={visible} animationType="slide" transparent>
+         <View style={styles.modalOverlay}>
+        <View style={styles.modalContent}>
+          <Text style={styles.modalTitle}>Adicionar Equipe</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Nome da equipe"
+            value={value}
+            onChangeText={onChange}
+          />
+          <View style={styles.modalButtons}>
+            <Pressable onPress={onSave} style={styles.modalButton}>
+              <Text>Salvar</Text>
+            </Pressable>
+            <Pressable onPress={onClose} style={styles.modalButton}>
+              <Text>Cancelar</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "#000000aa",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    width: "90%",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 22,
+    marginBottom: 15,
+    fontWeight: "bold",
+  },
+  input: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 6,
+    padding: 10,
+    marginBottom: 10,
+  },
+  modalButtons: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    marginTop: 10,
+  },
+  modalButton: {
+    padding: 10,
+    backgroundColor: "#ddd",
+    borderRadius: 6,
+  },
+});
+
+
